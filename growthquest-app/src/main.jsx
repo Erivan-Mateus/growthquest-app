8b21f5878ba2633ac1922d1b7277fc2654905122
Erivan Mateus <erivanmateussantosdesouza@gmail.com>
2026-08-30 10:38:24 -0300

    Initial commit

---
+++ growthquest-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GrowthQuestApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GrowthQuestApp />
+  </React.StrictMode>
+);
