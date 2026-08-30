ec53ead3373a717328da2e7cd4052e3fd4d42246
Erivan Mateus <erivanmateussantosdesouza@gmail.com>
2026-08-30 10:49:51 -0300

    Delete growthquest-app directory

--- growthquest-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GrowthQuestApp from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GrowthQuestApp />
-  </React.StrictMode>
-);
